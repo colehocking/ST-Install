9f5ce7350dcc462e412866610cdf2505181dfe56
Cole Hocking <colehocking1@gmail.com>
2022-02-18 12:06:25 -0700

    keeping token for template

--- Server/run-server.sh
+++ Server/run-server.sh
@@ -4,8 +4,7 @@
 
 #ID Token for Players to use
 
-
-TOKEN="login_token_here"
+TOKEN="connect_token_here"
 
 # Run the docker instance; install via docker-install-ubuntu.sh
 sudo docker run -d -p 10578:10578/udp tiltedphoques/st-game-server -premium -token "${TOKEN}"
